c3c22d3ae1b98ba95aadeef3794fbc3e354b4906
pedromnchunks35 <pedrosilvamnchunks@gmail.com>
2023-06-26 14:31:46 +0100

    Withdraw tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,24 +1,89 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 import "../node_modules/hardhat/console.sol";
-contract Exchange{
-//? EXCHANGE FUNCTIONS
-//* Deposit tokens
-//* Withdrawl tokens
-//* Check balances
-//* Make orders
-//* Cancel orders
-//* Fill Orders
-//* Charge Fees
-//* Track Fee Account
-    
+import "./Token.sol";
+
+contract Exchange {
+    //? EXCHANGE FUNCTIONS
+    //* Deposit tokens
+    //* Withdrawl tokens
+    //* Check balances
+    //* Make orders
+    //* Cancel orders
+    //* Fill Orders
+    //* Charge Fees
+    //* Track Fee Account
+
     //? ACCOUNT THAT WILL RECEIVE THE FEE FROM THE EXCHANGE OF TOKENS
     address public feeAccount;
+
     //? THE PERCENTAGE THAT WE WILL CHARGE TO TRADE TOKENS
     uint256 public feePercent;
 
-    constructor(address _feeAccount, uint256 _feePercent){
+    //? CREATE THE MAPPING TO TRACK WHICH TOKENS THE USER DEPOSIT, it takes the user address, the address of the token and the amount of tokens he deposited once uppon the time
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    //? CONSTRUCT FUNCTION, THIS WILL INITIATE THE CONTRACT
+    constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    //! EVENTS
+    event Deposit(
+        address _token,
+        address _user,
+        uint256 _amount,
+        uint256 _balance
+    );
+    event Withdraw(
+        address _token,
+        address _user,
+        uint256 _amount,
+        uint256 _balance
+    );
+
+    //? DEPOSIT TOKEN
+    //* @param _token, address of the token we want to deposit
+    //* @param _amount, the amount of tokens we want to deposit
+    //* @returns Success or Revert
+    //* @notes, address(this) is how we get the address of this smart contract
+    function depositToken(address _token, uint256 _amount) public {
+        // TODO Transfer tokens to exchange smart contract (YES WE CAN USE THE REQUIRE WITH A OPERATION FOR CASE IT FAILS WE GIVE A CERTAIN REASON)
+        Token(_token).transferFrom(msg.sender, address(this), _amount);
+
+        // TODO Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+        // TODO Emit an event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    //? CHECK BALANCES
+    //* @param _token, which is the address of the token that we want to check the balance
+    //* param _user, the address of the user that once deposited a certain amount
+    function balanceOf(
+        address _token,
+        address _user
+    ) public view returns (uint256) {
+        return tokens[_token][_user];
+    }
+
+    //? WITHDRAW TOKENS
+    //* @param _token, the address of the token we want to withdraw
+    //* @param _amount, the amount of tokens we want to withdraw
+    //* @returns Success or Revert
+    function withdrawToken(address _token, uint256 _amount) public {
+        //TODO Balance verification
+        require(
+            balanceOf(_token, msg.sender) >= _amount,
+            "You don't have enought balance of that token to withdraw"
+        );
+        // TODO Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+        // TODO Transfer token to the user
+        Token(_token).transfer(msg.sender, _amount);
+        // TODO Emit event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -3,31 +3,177 @@ const { ethers } = require("hardhat");
 /* FUNCTION TO TAKE A NUMBER OF TOKENS AND HAVE THAT
 CONSIDERING THE DECIMALS */
 const realVal = (_number_of_tokens) => {
+
     return ethers.utils.parseUnits(_number_of_tokens.toString(), 'ether');
+
 }
+
 describe('Exchange', () => {
-    let deployer, feeAccount, exchange;
+    let deployer, feeAccount, exchange, tokenDapp, tokenTwo, user1;
     let feePercent = 10;
+    let valueSent = realVal(100);
+
     //FUNCTION TO RUN BEFORE RUNNING THE TESTS
     beforeEach(async () => {
+        const Exchange = await ethers.getContractFactory('Exchange');
+        const Token = await ethers.getContractFactory('Token');
+        const Token2 = await ethers.getContractFactory('Token');
+        tokenDapp = await Token.deploy('Dapp University', 'DAPP', '1000000');
         accounts = await ethers.getSigners();
         deployer = accounts[0];
         feeAccount = accounts[1];
-        const Exchange = await ethers.getContractFactory('Exchange');
-        exchange = await Exchange.deploy(feeAccount.address,feePercent);
+        user1 = accounts[2];
+        exchange = await Exchange.deploy(feeAccount.address, feePercent);
+        //? TRANSFER 100 TOKENS TO THE ACCOUNT WE ARE TESTING OUT LATER
+        let transaction = await tokenDapp.connect(deployer).transfer(user1.address, valueSent);
     });
+
     describe('Deployment', () => {
+
         it("Tracks the fee account", () => {
             exchange.feeAccount()
                 .then(feeAcc => {
                     expect(feeAcc).to.equal(feeAccount);
                 })
         });
-        it("Tracks the fee percent",()=>{
+
+        it("Tracks the fee percent", () => {
             exchange.feePercent()
-            .then(feeP=>{
-                expect(feeP).to.equal(feePercent);
-            })
+                .then(feeP => {
+                    expect(feeP).to.equal(feePercent);
+                });
         })
+
     });
-})
+
+    describe('Depositing tokens', () => {
+
+        describe('Success', () => {
+            let result;
+            let amount = realVal(10);
+
+            beforeEach(() => {
+                //? MAKE THE APPROVE IN THE TOKEN DAPP TO ALLOW THE CONTRACT TO TRANSFER THE TOKENS TO HIM-SELF ON HIS BEHALF
+                return tokenDapp.connect(user1)
+                    .approve(exchange.address, amount)
+                    .then(approve_tx => {
+                        return approve_tx.wait()
+                            .then(approve_result => {
+                                //? MAKE THE DEPOSIT
+                                return exchange.connect(user1)
+                                    .depositToken(tokenDapp.address, amount)
+                                    .then(deposit_tx => {
+                                        return deposit_tx.wait()
+                                            .then(deposit_result => {
+                                                result = deposit_result;
+                                            })
+                                    });
+                            });
+                    });
+            });
+
+            it("Tracks the token deposit", () => {
+                //? CHECK THE EXCHANGE BALANCE OF DAP TOKENS
+                return tokenDapp.balanceOf(exchange.address)
+                    .then(exchangeBalance => {
+                        expect(exchangeBalance).to.equal(amount);
+                        //? CHECK THE DEPOSITS OF THE USER
+                        return exchange.tokens(tokenDapp.address, user1.address)
+                            .then(exchangeBalanceUser1 => {
+                                expect(exchangeBalanceUser1).to.equal(amount);
+                            });
+                    });
+            });
+
+            it('Emits a Deposit event', () => {
+                const event = result.events[1]; //IT IS THE SECOUND EVENT
+                expect(event.event).to.equal('Deposit');
+                const args = event.args;
+                expect(args._token).to.equal(tokenDapp.address);
+                expect(args._user).to.equal(user1.address);
+                expect(args._amount).to.equal(amount);
+                expect(args._balance).to.equal(amount);
+            });
+        });
+
+        describe('Failure', () => {
+            let amount = realVal(10);
+            it('Fails when no tokens are approved', () => {
+                return exchange.connect(user1)
+                    .depositToken(tokenDapp.address, amount)
+                    .then()
+                    .catch(err => {
+                        expect(err.message).to.include("revert");
+                        expect(err.message).to.include("You don't have enough allowance");
+                    })
+            });
+        });
+    });
+
+    describe("Withdrawing Tokens", () => {
+        let transaction, result;
+        let amount = realVal(10);
+        describe('Success', () => {
+            beforeEach(() => {
+                //? MAKE THE APPROVE IN THE TOKEN DAPP TO ALLOW THE CONTRACT TO TRANSFER THE TOKENS TO HIM-SELF ON HIS BEHALF
+                return tokenDapp.connect(user1)
+                    .approve(exchange.address, amount)
+                    .then(approve_tx => {
+                        return approve_tx.wait()
+                            .then(approve_result => {
+                                //? MAKE THE DEPOSIT
+                                return exchange.connect(user1)
+                                    .depositToken(tokenDapp.address, amount)
+                                    .then(deposit_tx => {
+                                        return deposit_tx.wait()
+                                            .then(deposit_result => {
+                                                //? MAKE THE WITHDRAW
+                                                return exchange.connect(user1)
+                                                    .withdrawToken(tokenDapp.address, amount)
+                                                    .then(tx_withdraw => {
+                                                        return tx_withdraw.wait()
+                                                            .then(withdraw_result => {
+                                                                result = withdraw_result;
+                                                            })
+                                                    })
+                                            })
+                                    });
+                            });
+                    });
+
+            });
+            it("Withaws token funds", () => {
+                return exchange.connect(user1)
+                    .balanceOf(tokenDapp.address, user1.address)
+                    .then(user1_balance_exchange => {
+                        expect(user1_balance_exchange).to.equal(0);
+                        return tokenDapp.connect(user1)
+                            .balanceOf(user1.address)
+                            .then(user1_balance_tokenDapp => {
+                                expect(user1_balance_tokenDapp).to.equal(valueSent);
+                            })
+                    })
+            });
+            it("Emits a withdraw event", () => {
+                const event = result.events[1];
+                expect(event.event).to.equal("Withdraw");
+                const args = event.args;
+                expect(args._token).to.equal(tokenDapp.address);
+                expect(args._user).to.equal(user1.address);
+                expect(args._amount).to.equal(amount);
+                expect(args._balance).to.equal(0);
+            });
+        });
+        describe("Failure", () => {
+            it("Reverts when there is no balance", () => {
+                return exchange.connect(user1)
+                    .withdrawToken(tokenDapp.address, realVal(100))
+                    .then()
+                    .catch(err => {
+                        expect(err.message).to.include("revert");
+                        expect(err.message).to.include("You don't have enought balance of that token to withdraw");
+                    });
+            })
+        });
+    });
+});
