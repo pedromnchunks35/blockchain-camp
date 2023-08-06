693eb91e2f7ef6d1354a0bd5407e818eacec8789
Pedro Silva <pedrosilvamnchunks@gmail.com>
2023-08-06 17:44:47 +0100

    Price chart done 80% tut

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -1,18 +1,28 @@
 import { useSelector } from "react-redux";
+import Chart from 'react-apexcharts'
 import { Banner } from "./Banner";
+import { options, defaultSeries } from "./PriceChart.config";
+import { priceChartSelector } from "../store/selectors";
+import arrowDown from '../assets/down-arrow.svg'
+import arrowUp from '../assets/up-arrow.svg'
 const PriceChart = () => {
-  const account = useSelector(state=>state.provider.account)
+  const account = useSelector(state => state.provider.account)
+  const symbols = useSelector(state => state.tokens.symbols)
+  const priceChart = useSelector(priceChartSelector)
   return (
     <div className="component exchange__chart">
       <div className='component__header flex-between'>
         <div className='flex'>
 
-          <h2></h2>
-
-          <div className='flex'>
-            <img src="" alt="Arrow down" />
-            <span className='up'></span>
-          </div>
+          <h2>{symbols && `${symbols[0]}/${symbols[1]}`}</h2>
+          {
+            priceChart && (
+              <div className='flex'>
+                <img src={priceChart.lastPriceChange == "+" ? arrowUp : arrowDown} alt="Arrow down" />
+                <span className='up'>{priceChart.lastPrice}</span>
+              </div>
+            )
+          }
 
         </div>
       </div>
@@ -20,12 +30,18 @@ const PriceChart = () => {
       {/* Price chart goes here */}
       {
         !account ?
-        (
-         <Banner text={"Please connect the wallet"}/>
-        ):
-        (
-          <Banner text={"Connected"}/>
-        )
+          (
+            <Banner text={"Please connect the wallet"} />
+          ) :
+          (
+            <Chart
+              type="candlestick"
+              options={options}
+              series={priceChart ? priceChart.series : defaultSeries}
+              width="100%"
+              height="100%"
+            />
+          )
       }
 
     </div>
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,25 +1,25 @@
 import { createSelector } from 'reselect'
-import { get, groupBy,reject } from 'lodash'
+import { get, groupBy, reject, sortBy, maxBy, minBy, last } from 'lodash'
 import moment from 'moment'
-import {ethers} from 'ethers'
+import { ethers } from 'ethers'
 //? High level of accessing variables using loadash
 const tokens = state => get(state, 'tokens.contracts')
 const allOrders = state => get(state, 'exchange.allOrders.data', [])
 const GREEN = '#25CE8F'
 const RED = '#F45353'
-const cancelledOrders = state => get(state,'exchange.cancelledOrders.data',[])
-const filledOrders = state => get(state,'exchange.filledOrders.data',[])
+const cancelledOrders = state => get(state, 'exchange.cancelledOrders.data', [])
+const filledOrders = state => get(state, 'exchange.filledOrders.data', [])
 //? function to get only the open orders, removing the filled and cancelled ones
-const openOrders = state =>{
+const openOrders = state => {
     //? Grab all the data
     const all = allOrders(state)
     const filled = filledOrders(state)
     const cancelled = filledOrders(state)
     //? reject filled and cancelled
-    const openOrders = reject(all,(order)=>{
-        const orderFilled = filled.some((o)=>o._id.toString()===order._id.toString())
-        const orderCancelled = cancelled.some((o)=>o._id.toString()===order._id.toString())
-        return(orderFilled || orderCancelled)
+    const openOrders = reject(all, (order) => {
+        const orderFilled = filled.some((o) => o._id.toString() === order._id.toString())
+        const orderCancelled = cancelled.some((o) => o._id.toString() === order._id.toString())
+        return (orderFilled || orderCancelled)
     })
     return openOrders
 }
@@ -54,20 +54,19 @@ export const orderBookSelector = createSelector(
         orders = orders.filter((o) => o._tokenGet === tokens[0].address || o._tokenGet === tokens[1].address)
         orders = orders.filter((o) => o._tokenGive === tokens[0].address || o._tokenGive === tokens[1].address)
         orders = decorateOrderBookOrders(orders, tokens)
-        orders = groupBy(orders,'orderType')
+        orders = groupBy(orders, 'orderType')
         //? sort orders by token price
-        const buyOrders = get(orders,'buy',[])
+        const buyOrders = get(orders, 'buy', [])
         orders = {
             ...orders,
-            buyOrders: buyOrders.sort((a,b)=>b.tokenPrice-a.tokenPrice)
+            buyOrders: buyOrders.sort((a, b) => b.tokenPrice - a.tokenPrice)
         }
         //? sort sell orders by token price
-        const sellOrders = get(orders,'sell',[])
+        const sellOrders = get(orders, 'sell', [])
         orders = {
             ...orders,
-            sellOrders: sellOrders.sort((a,b)=>b.tokenPrice-a.tokenPrice)
+            sellOrders: sellOrders.sort((a, b) => b.tokenPrice - a.tokenPrice)
         }
-        console.log(orders)
         return orders
     }
 )
@@ -90,8 +89,64 @@ const decorateOrderBookOrder = (order, tokens) => {
         {
             ...order,
             orderType,
-            orderTypeClass: (orderType==='buy'?GREEN:RED),
-            orderFillAction: (orderType === 'buy'?'sell':'buy')
+            orderTypeClass: (orderType === 'buy' ? GREEN : RED),
+            orderFillAction: (orderType === 'buy' ? 'sell' : 'buy')
         }
     )
+}
+
+//? Price chart selectors
+export const priceChartSelector = createSelector(
+    filledOrders,
+    tokens,
+    (orders, tokens) => {
+        if (!tokens[0] || !tokens[1]) { return }
+        //? Filter the data
+        orders = orders.filter((o) => o._tokenGet === tokens[0].address || o._tokenGet === tokens[1].address)
+        orders = orders.filter((o) => o._tokenGive === tokens[0].address || o._tokenGive === tokens[1].address)
+        //? sort orders by timestamp
+        orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+        //? Decorate the orders
+        orders = orders.map((o) => decorateOrder(o, tokens))
+        let secoundLastOrder, lastOrder
+        [secoundLastOrder, lastOrder] = orders.slice(orders.length - 2, orders.length)
+        const lastPrice = get(lastOrder, 'tokenPrice', 0)
+        const secoundLastPrice = get(secoundLastOrder, 'tokenPrice', 0)
+        let result = (buildGraphData(orders))
+        result.lastPrice = lastPrice
+        result.lastPriceChange = (lastPrice >= secoundLastPrice ? "+" : "-")
+        return result
+    }
+)
+
+const buildGraphData = (orders) => {
+    //? Order by timestamp
+    orders = groupBy(orders, (o) =>
+        //? Group it by day
+        moment.unix(o._timestamp).startOf('hour').format()
+    )
+    const hours = Object.keys(orders)
+    const graphData = hours.map((hour) => {
+        //? Fetch all orders from current hour
+        const group = orders[hour]
+        //? Calculate the prices
+        //? open is the first because we shorted it by timestamp
+        const open = group[0]
+        //? high will be the maximum price of this group
+        const high = maxBy(group, "tokenPrice")
+        //? low will be the lowest price of this group
+        const low = minBy(group, "tokenPrice")
+        //? close will be the last price of that group
+        const close = group[group.length - 1]
+        return ({
+            x: new Date(hour),
+            y: [open.tokenPrice, high.tokenPrice, low.tokenPrice, close.tokenPrice]
+        })
+    })
+
+    return {
+        series: [{
+            data: graphData
+        }]
+    }
 }
