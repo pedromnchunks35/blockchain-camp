868c0bb4da4dcfeec3b3653c5b31cb3937632c15
Pedro Silva <pedrosilvamnchunks@gmail.com>
2023-08-19 19:11:25 +0100

    Put another chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config()
+//? add the private keys
+const privateKeys = process.env.PRIVATE_KEYS || ""
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     /* BY LEAVING IT EMPTY IT KNOW THAT IT IS A LOCAL NETWORK */
-    localhost: {}
+    localhost: {},
+    auroratestnet: {
+      url: "https://aurora-testnet.infura.io/v3/"+process.env.INFURA_API_KEY,
+      accounts: privateKeys.split(","),
+    }
   },
   hardhat: {
     stackTrace: {
